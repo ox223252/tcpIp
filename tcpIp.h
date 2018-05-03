39f814a68c977708222494b595ef2973a5332c57
ox223252 <38324847+ox223252@users.noreply.github.com>
2018-05-03 21:08:06 +0200

    Update licence agrement

--- tcpIp.h
+++ tcpIp.h
@@ -1,13 +1,29 @@
-#ifndef __SERVERTCP_H__
-#define __SERVERTCP_H__
+#ifndef __TCPIP_H__
+#define __TCPIP_H__
+////////////////////////////////////////////////////////////////////////////////
+/// \copiright ox223252, 2018
+///
+/// This program is free software: you can redistribute it and/or modify it
+///     under the terms of the GNU General Public License published by the Free
+///     Software Foundation, either version 2 of the License, or (at your
+///     option) any later version.
+///
+/// This program is distributed in the hope that it will be useful, but WITHOUT
+///     ANY WARRANTY; without even the implied of MERCHANTABILITY or FITNESS FOR
+///     A PARTICULAR PURPOSE. See the GNU General Public License for more
+///     details.
+///
+/// You should have received a copy of the GNU General Public License along with
+///     this program. If not, see <http://www.gnu.org/licenses/>
+////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-/// \file serverTcp.h
+/// \file tcpIp.h
 /// \brief library to create and use a basic TCP server to send and received 
 ///     datas
 /// \author ox223252
 /// \date 2017-07
-/// \copyright LGPL
+/// \copyright GPLv2
 /// \version 0.1
 /// \warning NONE
 /// \bug NONE
